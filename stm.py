d30cf0583654f1cee86c6f108e9892badb314663
islem <56887648+islemSmai@users.noreply.github.com>
2022-12-13 01:38:29 +0100

    Add files via upload

--- stm.py
+++ stm.py
@@ -1,8 +1,6 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 
 st.title('face detection')
 st.markdown('Toy model to play to classify iris flowers into \ setosa, versicolor, virginica')
